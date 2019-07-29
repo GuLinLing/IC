547d8c890fb8ba37b26bae919875d94a7f11fd44
顾林岭 <gulinling1997@qq.com>
2019-07-29 17:52:43 +0800

    Create Page-1.vue

---
+++ Page-1.vue
@@ -0,0 +1,20 @@
+<style scoped>
+</style>
+
+<template>
+  <section class="page-1">
+    <Trick>
+      <img src="@/assets/img/1.png">
+    </Trick>
+  </section>
+</template>
+
+<script>
+  import Trick from '@/components/Trick.vue'
+  export default {
+    name: 'Page1',
+    components: {
+      [Trick.name]: Trick
+    }
+  }
+</script>
