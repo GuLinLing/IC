647e2e504ac9d8e874ee7f77048cd250562260ad
顾林岭 <gulinling1997@qq.com>
2019-07-29 17:48:42 +0800

    Create Trick.vue

---
+++ Trick.vue
@@ -0,0 +1,32 @@
+<style scoped>
+  .trick {
+    position: relative;
+    display: inline-block;
+  }
+  .active img {
+    animation: blurry 10s ease infinite 1s;
+  }
+  @keyframes blurry {
+    0% {
+      filter: blur(0px);
+    }
+    5% {
+      filter: blur(10px);
+    }
+    10% {
+      filter: blur(0px);
+    }
+  }
+</style>
+
+<template>
+  <div class="trick">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Trick'
+  }
+</script>
