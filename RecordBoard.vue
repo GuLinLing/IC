c05f319c5bca74c4c3c409464667af725f986fd6
顾林岭 <gulinling1997@qq.com>
2019-07-29 17:49:39 +0800

    Create RecordBoard.vue

---
+++ RecordBoard.vue
@@ -0,0 +1,49 @@
+<style scoped>
+  .record-board {
+    width: 100%;
+    margin: 0 calc((100vw / 150) + 20px);
+    animation: hide 0.5s ease forwards;
+  }
+  .active .record-board {
+    animation: show 1s ease forwards;
+  }
+  @keyframes hide {
+    0% {
+      opacity: 1;
+      transform: rotateZ(0deg) scale(1);
+    }
+    100% {
+      opacity: 0;
+      transform: rotateZ(999deg) scale(0);
+    }
+  }
+  @keyframes show {
+    0% {
+      opacity: 0;
+      transform: rotateZ(999deg) scale(0);
+    }
+    100% {
+      opacity: 1;
+      transform: rotateZ(0deg) scale(1);
+    }
+  }
+</style>
+
+<template>
+  <div class="record-board">
+    <RecordBoardHeader></RecordBoardHeader>
+    <RecordBoardMain></RecordBoardMain>
+  </div>
+</template>
+
+<script>
+  import RecordBoardHeader from './RecordBoardHeader.vue'
+  import RecordBoardMain from './RecordBoardMain.vue'
+  export default {
+    name: 'RecordBoard',
+    components: {
+      [RecordBoardHeader.name]: RecordBoardHeader,
+      [RecordBoardMain.name]: RecordBoardMain
+    }
+  }
+</script>
